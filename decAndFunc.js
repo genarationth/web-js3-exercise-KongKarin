bc36c38c7da65e8f4490d7d15863ea2fe668320c
PJK <PJK@gmail.com>
2023-03-16 09:53:22 +0700

    addansw

--- decAndFunc.js
+++ decAndFunc.js
@@ -5,11 +5,6 @@
 
 // Return the value of what she should be paying.
 
-// const transation = (cost1, cost2, cost3)
-// const fee = 3
-// const interestFee = 0.01
-
-
 
 // let totalT = 1000, 15000
 const transactions = [100, 200, 300, 400, 500];
@@ -30,3 +25,53 @@ const totalCost = feeTotal + interestTotal;
 const paymentAmount = transactionTotal + totalCost;
 
 console.log("Maria's total payment amount for the month is $" + paymentAmount.toFixed(2));
+
+
+// ### Exercise #2
+
+// #### Part 1
+// Ed would like a way to input 3 names of his friends.
+// The output should be a console greeting to his friends saying: 
+// `Welcome {name1}, {name2}, {name3}.`
+
+console.log(`Welcome 'ben', 'ban', 'benny'`);
+
+
+
+// #### Part 2
+// Ed would like to create a function that takes in a birth year and returns the age.
+
+// i.e. 1990 returns 30
+
+
+// console.log(birthday);
+
+// #### Part 3
+// Ed would like to create a function that prints out,
+// `Welcome {name1}, you are {age1}. Welcome {name2}, you are {age2}. Welcome {name3}, you are {age3}.`
+
+let friendName1;
+let friendName2;
+let friendName3;
+let friendYear1;
+let friendYear2;
+let friendYear3;
+// function welcome + friend name
+function hiFriend (friendName1,friendName2,friendName3,) {
+    console.log('Welcome ' + friendName1+', '+friendName2+', '+friendName3+'.');
+}
+hiFriend('Pop','Mark','Joe');
+​
+// cal friend age
+function ageFriend (friendYear1,friendYear2,friendYear3) {
+    console.log(2023 - friendYear1);
+    console.log(2023 - friendYear2);
+    console.log(2023 - friendYear3);
+}
+ageFriend(1999,1998,1997);
+
+// combine name & age 
+function welcomeFriend (friendName1,friendName2,friendName3,friendYear1,friendYear2,friendYear3) {
+    console.log(`Welcome ${friendName1}, you are ${friendYear1}. Welcome ${friendName2}, you are ${friendYear2}. Welcome ${friendName3}, you are ${friendYear3}.`);
+}
+welcomeFriend('Pop','Mark','Joe',1999,1998,1997);
